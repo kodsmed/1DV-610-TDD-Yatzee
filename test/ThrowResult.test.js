ad13dfba097b868d8c8f80831e537658156d99bf
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-12 17:44:31 +0100

    RED: ThrowResult, fix broken mock implementation

--- test/ThrowResult.test.js
+++ test/ThrowResult.test.js
@@ -1,16 +1,26 @@
+import { expect, jest } from '@jest/globals'
 import ThrowResult from '../serve/compiled-js/classes/ThrowResult.js';
 
+// Mocking the Die class so we can control the faceValue
+const Die = jest.fn().mockImplementation((value) => {
+  return {
+    faceValue: value,
+    roll: function() {
+      return value;
+    }
+  }
+});
+
+const die1 = Die(1)
+const die2 = Die(2);
+const die3 = Die(3);
+const die4 = Die(4);
+const die5 = Die(5);
+const die6 = Die(6);
+
+const diceArray = [die1, die2, die3, die4, die5, die6];
+
 describe('Throw', () => {
-  // Mock the dies 1-6
-  const die1 = { faceValue: jest.fn().mockReturnValue(1) };
-  const die2 = { faceValue: jest.fn().mockReturnValue(2) };
-  const die3 = { faceValue: jest.fn().mockReturnValue(3) };
-  const die4 = { faceValue: jest.fn().mockReturnValue(4) };
-  const die5 = { faceValue: jest.fn().mockReturnValue(5) };
-  const die6 = { faceValue: jest.fn().mockReturnValue(6) };
-
-  const diceArray = [die1, die2, die3, die4, die5, die6];
-
 
   it('should be defined', () => {
     expect(ThrowResult).toBeDefined();
@@ -41,20 +51,20 @@ describe('Throw', () => {
   it('should return the same numbers as the dies', () => {
     let throwObj = new ThrowResult(diceArray);
     let rollResult = throwObj.rollResult;
-    expect(rollResult[0]).toBe(die1.faceValue());
-    expect(rollResult[1]).toBe(die2.faceValue());
-    expect(rollResult[2]).toBe(die3.faceValue());
-    expect(rollResult[3]).toBe(die4.faceValue());
-    expect(rollResult[4]).toBe(die5.faceValue());
-    expect(rollResult[5]).toBe(die6.faceValue());
+    expect(rollResult[0]).toBe(die1.faceValue);
+    expect(rollResult[1]).toBe(die2.faceValue);
+    expect(rollResult[2]).toBe(die3.faceValue);
+    expect(rollResult[3]).toBe(die4.faceValue);
+    expect(rollResult[4]).toBe(die5.faceValue);
+    expect(rollResult[5]).toBe(die6.faceValue);
 
     throwObj = new ThrowResult([die6, die5, die4, die3, die2, die1]);
     rollResult = throwObj.rollResult;
-    expect(rollResult[0]).toBe(die6.faceValue());
-    expect(rollResult[1]).toBe(die5.faceValue());
-    expect(rollResult[2]).toBe(die4.faceValue());
-    expect(rollResult[3]).toBe(die3.faceValue());
-    expect(rollResult[4]).toBe(die2.faceValue());
-    expect(rollResult[5]).toBe(die1.faceValue());
+    expect(rollResult[0]).toBe(die6.faceValue);
+    expect(rollResult[1]).toBe(die5.faceValue);
+    expect(rollResult[2]).toBe(die4.faceValue);
+    expect(rollResult[3]).toBe(die3.faceValue);
+    expect(rollResult[4]).toBe(die2.faceValue);
+    expect(rollResult[5]).toBe(die1.faceValue);
   });
 });
