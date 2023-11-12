7af27b200ea441f93d144f9ceb35d78a1df275da
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-12 22:36:12 +0100

    GREEN: StraitsScoreStrategy

--- src/classes/StraitsScoreStrategy.ts
+++ src/classes/StraitsScoreStrategy.ts
@@ -2,11 +2,25 @@ import ThrowResult from "./ThrowResult.js";
 import ScoreStrategy from "../ScoreStrategy.js";
 
 export default class StraitsScoreStrategy implements ScoreStrategy {
+  #expectedStrait: Array<number>;
+  #throwResult: ThrowResult;
+
   constructor(expectedStrait: Array<number>, throwResult: ThrowResult) {
-    throw new Error("Method not implemented.");
+    this.#expectedStrait = expectedStrait;
+    this.#throwResult = throwResult;
   }
 
   get score(): number {
-    throw new Error("Method not implemented.");
+    let allExpectedExists = true;
+    let score = 0;
+    for (const expectedNumber of this.#expectedStrait) {
+      if (!this.#throwResult.rollResult.includes(expectedNumber)) {
+        allExpectedExists = false;
+        break;
+      } else {
+        score += expectedNumber;
+      }
+    }
+    return allExpectedExists ? score : 0;
   }
 }
