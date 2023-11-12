6fdce13cb2d7c55471d571f5bf1c01e4a8b2305d
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-12 16:35:25 +0100

    GREEN: DieClass, passed

--- src/classes/DieClass.ts
+++ src/classes/DieClass.ts
@@ -1,11 +1,21 @@
+export enum DieState {
+  Active = 'Active',
+  Held = 'Held'
+}
+
 export default class Die {
   #faceValue: number;
+  #state: DieState;
 
   constructor(){
     this.#faceValue = this.roll();
+    this.#state = DieState.Active;
   }
 
   roll(): number {
+    if (this.#state === DieState.Held) {
+      return Number(this.#faceValue);
+    }
     const rollResult = Math.floor(Math.random() * 6) + 1;
     this.#faceValue = rollResult;
     return rollResult;
@@ -13,5 +23,13 @@ export default class Die {
 
   get faceValue(): number {
     return Number(this.#faceValue);
+  }
+
+  get state(): DieState {
+    return this.#state;
+  }
+
+  setState(newState: DieState) {
+    this.#state = newState;
   }
 }
