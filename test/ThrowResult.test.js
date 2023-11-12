885c0d2d94c6894c1342a382af88f1d16e5e0232
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-12 17:08:03 +0100

    RED: ThrowResult, use an array instead of six arguments... what is this? the 20's?

--- test/ThrowResult.test.js
+++ test/ThrowResult.test.js
@@ -9,35 +9,37 @@ describe('Throw', () => {
   const die5 = { faceValue: jest.fn().mockReturnValue(5) };
   const die6 = { faceValue: jest.fn().mockReturnValue(6) };
 
+  const diceArray = [die1, die2, die3, die4, die5, die6];
+
 
   it('should be defined', () => {
     expect(ThrowResult).toBeDefined();
   });
 
   it('should construct a new throw', () => {
-    const throwObj = new ThrowResult(die1, die2, die3, die4, die5, die6);
+    const throwObj = new ThrowResult(diceArray);
     expect(throwObj).toBeDefined();
   });
 
   it('should return an array', () => {
-    const throwObj = new ThrowResult(die1, die2, die3, die4, die5, die6);
+    const throwObj = new ThrowResult(diceArray);
     expect(Array.isArray(throwObj.rollResult)).toBe(true);
   });
 
   it('should return an array of numbers', () => {
-    const throwObj = new ThrowResult(die1, die2, die3, die4, die5, die6);
+    const throwObj = new ThrowResult(diceArray);
     const rollResult = throwObj.rollResult;
     expect(rollResult.every((value) => typeof value === 'number')).toBe(true);
   });
 
   it('should return an array of numbers between 1 and 6', () => {
-    const throwObj = new ThrowResult(die1, die2, die3, die4, die5, die6);
+    const throwObj = new ThrowResult(diceArray);
     const rollResult = throwObj.rollResult;
     expect(rollResult.every((value) => value >= 1 && value <= 6)).toBe(true);
   });
 
   it('should return the same numbers as the dies', () => {
-    let throwObj = new ThrowResult(die1, die2, die3, die4, die5, die6);
+    let throwObj = new ThrowResult(diceArray);
     let rollResult = throwObj.rollResult;
     expect(rollResult[0]).toBe(die1.faceValue());
     expect(rollResult[1]).toBe(die2.faceValue());
@@ -46,7 +48,7 @@ describe('Throw', () => {
     expect(rollResult[4]).toBe(die5.faceValue());
     expect(rollResult[5]).toBe(die6.faceValue());
 
-    throwObj = new ThrowResult(die6, die5, die4, die3, die2, die1);
+    throwObj = new ThrowResult([die6, die5, die4, die3, die2, die1]);
     rollResult = throwObj.rollResult;
     expect(rollResult[0]).toBe(die6.faceValue());
     expect(rollResult[1]).toBe(die5.faceValue());
