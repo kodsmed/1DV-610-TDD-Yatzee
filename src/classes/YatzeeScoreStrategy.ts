96c09c6c51cf0df6c8551f358c0b6c5fb2b1ca5a
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-12 21:53:37 +0100

    GREEN: YatzeeScoreStrategy

--- src/classes/YatzeeScoreStrategy.ts
+++ src/classes/YatzeeScoreStrategy.ts
@@ -2,11 +2,15 @@ import ThrowResult from "./ThrowResult.js";
 import ScoreStrategy from "../ScoreStrategy.js";
 
 export default class YatzeeScoreStrategy implements ScoreStrategy {
+  #throwResult: ThrowResult;
+
   constructor(throwResult: ThrowResult) {
-    throw new Error("Method not implemented.");
+    this.#throwResult = throwResult;
   }
 
   get score(): number {
-    throw new Error("Method not implemented.");
+    const firstDie = this.#throwResult.rollResult[0];
+    const allDiceAreTheSame = this.#throwResult.rollResult.every(die => die === firstDie);
+    return allDiceAreTheSame ? 100 : 0;
   }
 }
