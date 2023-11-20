785c93359acadd312ba3770be64f503f25229a6d
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-20 23:36:33 +0100

    RED: Should return an empty array if no matches are found

--- test/Utiliity.test.js
+++ test/Utiliity.test.js
@@ -17,5 +17,13 @@ describe('Utility', () => {
       const result = utility.findOfAKind(arrayToSearch, requiredOfAKind);
       expect(result.every((value) => typeof value === 'number')).toBe(true);
     });
+
+    it ('should return an empty array if no matches are found', () => {
+      const utility = new Utility();
+      const arrayToSearch = [1, 2, 3, 4, 5, 6];
+      const requiredOfAKind = 3;
+      const result = utility.findOfAKind(arrayToSearch, requiredOfAKind);
+      expect(result.length).toBe(0);
+    });
   });
 });
