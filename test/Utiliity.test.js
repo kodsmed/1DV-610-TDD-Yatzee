7c2e0c4a0a95155279ac366a8d9e2bb5ab14d41a
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-20 23:42:26 +0100

    REd: should return an array of [1,2,3] if the array is [1, 1, 2, 2, 3, 3] and the requiredOfAKind is 2

--- test/Utiliity.test.js
+++ test/Utiliity.test.js
@@ -25,5 +25,13 @@ describe('Utility', () => {
       const result = utility.findOfAKind(arrayToSearch, requiredOfAKind);
       expect(result.length).toBe(0);
     });
+
+    it ('should return an array of [1,2,3] if the array is [1, 1, 2, 2, 3, 3] and the requiredOfAKind is 2', () => {
+      const utility = new Utility();
+      const arrayToSearch = [1, 1, 2, 2, 3, 3];
+      const requiredOfAKind = 2;
+      const result = utility.findOfAKind(arrayToSearch, requiredOfAKind);
+      expect(result).toEqual([1, 2, 3]);
+    });
   });
 });
