ca7b2da2c73120190762479e113d02b69f744020
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-20 23:29:50 +0100

    RED: Utility class

--- test/Utiliity.test.js
+++ test/Utiliity.test.js
@@ -1,12 +1,12 @@
-import Utility from '../src/classes/Utility.js';
+import Utility from '../serve/compiled-js/classes/Utility.js';
 
 describe('Utility', () => {
-  describe (findOfAKind, () => {
-    it('should return an array of numbers', () => {
-      const Utility = new Utility();
+  describe ('findOfAKind', () => {
+    it('should return an array', () => {
+      const utility = new Utility();
       const arrayToSearch = [1, 2, 3, 4, 5, 6];
       const requiredOfAKind = 3;
-      const result = findOfAKind(arrayToSearch, requiredOfAKind);
+      const result = utility.findOfAKind(arrayToSearch, requiredOfAKind);
       expect(Array.isArray(result)).toBe(true);
     });
   });
