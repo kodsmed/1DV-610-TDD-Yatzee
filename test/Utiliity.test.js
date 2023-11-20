22859bcb3a6bfe1a4bf6a29df9c5b3cb4eb19097
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-20 23:33:52 +0100

    red

--- test/Utiliity.test.js
+++ test/Utiliity.test.js
@@ -2,12 +2,20 @@ import Utility from '../serve/compiled-js/classes/Utility.js';
 
 describe('Utility', () => {
   describe ('findOfAKind', () => {
-    it('should return an array', () => {
+    it ('should return an array', () => {
       const utility = new Utility();
       const arrayToSearch = [1, 2, 3, 4, 5, 6];
       const requiredOfAKind = 3;
       const result = utility.findOfAKind(arrayToSearch, requiredOfAKind);
       expect(Array.isArray(result)).toBe(true);
+    });
+
+    it ('should return an empty array or an array of numbers', () => {
+      const utility = new Utility();
+      const arrayToSearch = [1, 2, 3, 4, 5, 6];
+      const requiredOfAKind = 3;
+      const result = utility.findOfAKind(arrayToSearch, requiredOfAKind);
+      expect(result.every((value) => typeof value === 'number')).toBe(true);
     });
   });
 });
