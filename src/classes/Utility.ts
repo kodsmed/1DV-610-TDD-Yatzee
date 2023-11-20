753bd0c6f34c08a8b4800969b6c5bfba01ce03d8
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-20 23:45:16 +0100

    Green: Utility Class

--- src/classes/Utility.ts
+++ src/classes/Utility.ts
@@ -1,5 +1,24 @@
 export default class Utility {
   findOfAKind(numbers: Array<number>, expectedOfAKind: number) : Array<number> {
-    return [];
+    const elementCount = new Map();
+    let groups = [] as Array<number>;
+
+    // Count the occurrences of each element
+    numbers.forEach(element => {
+      if (!elementCount.has(element)) {
+        elementCount.set(element, 0);
+      }
+      elementCount.set(element, elementCount.get(element) + 1);
+    });
+
+    // Find elements that have exactly 2 occurrences (a pair)
+    elementCount.forEach((count, element) => {
+      if (count >= 2) {
+        groups.push(element);
+      }
+    });
+
+    return groups;
+
   }
 }
