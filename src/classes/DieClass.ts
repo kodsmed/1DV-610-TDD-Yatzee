a9e72c6050055dc8cf0d99ca3ad068bc93b40b44
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-21 00:35:16 +0100

    refactor Die to accept dependency injected random generator for testing

--- src/classes/DieClass.ts
+++ src/classes/DieClass.ts
@@ -7,18 +7,30 @@ export default class Die {
   #faceValue: number;
   #state: DieState;
 
-  constructor(){
-    this.#faceValue = this.roll();
+  /**
+   * Creates a new Die instance.
+   * The randomCallback parameter is used to inject a random number generator for testing purposes,
+   * if no callback is provided, the default callback will be used.
+   * The default callback will return a random number between 1 and 6.
+   *
+   * @param randomCallback function to be used as a callback for the random number generator.
+   */
+  constructor(randomCallback: Function = () => {return (Math.floor(Math.random() * 6) + 1)}) {
+    this.#faceValue = this.roll(randomCallback);
     this.#state = DieState.Active;
   }
 
-  roll(): number {
+  roll(randomCallback: Function): number {
     if (this.#state === DieState.Held) {
       return Number(this.#faceValue);
     }
-    const rollResult = Math.floor(Math.random() * 6) + 1;
+    const rollResult = this.roller(randomCallback);
     this.#faceValue = rollResult;
     return rollResult;
+  }
+
+  private roller(randomCallback: Function = () => {return (Math.floor(Math.random() * 6) + 1)}): number {
+    return randomCallback();
   }
 
   get faceValue(): number {
