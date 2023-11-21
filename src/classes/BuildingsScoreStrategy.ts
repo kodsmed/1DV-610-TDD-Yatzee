c7d2ac157261efc165b6bf3e4b3a296411e9e1a6
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-21 01:07:58 +0100

    RED: BuildingsScoreStrategy

--- src/classes/BuildingsScoreStrategy.ts
+++ src/classes/BuildingsScoreStrategy.ts
@@ -1,3 +1,9 @@
+export enum BuildingsStrategyType {
+  House,
+  Villa,
+  Tower
+}
+
 export default class BuildingsScoreStrategy {
   constructor() {
     throw new Error("Not implemented");
--- test/BuildingsScoreStrategy.test.js
+++ test/BuildingsScoreStrategy.test.js
@@ -1,11 +1,16 @@
 
 import { it } from 'node:test';
-import BuildingsScoreStrategy from '../serve/compiled-js/classes/BuildingsScoreStrategy.js';
+import BuildingsScoreStrategy, { BuildingsStrategyType } from '../serve/compiled-js/classes/BuildingsScoreStrategy.js';
 
 describe('BuildingsScoreStrategy', () => {
   it('should return a number', () => {
-    const scoreStrategy = new BuildingsScoreStrategy([1, 2, 3, 4, 5, 6]);
+    const scoreStrategy = new BuildingsScoreStrategy(
+      BuildingsStrategyType.House,
+      [1, 2, 3, 4, 5, 6]
+    );
     const score = scoreStrategy.score;
     expect(typeof score).toBe('number');
   });
+
+
 });
