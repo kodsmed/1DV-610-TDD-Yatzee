0079b58cd579a05fb8a758f8dfc2797423d298ee
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-21 22:13:47 +0100

    Refactor

--- test/NullScoreStrategy.test.js
+++ test/NullScoreStrategy.test.js
@@ -2,12 +2,6 @@ import NullScoreStrategy from '../serve/compiled-js/classes/NullScoreStrategy.js
 
 describe('NullScoreStrategy', () => {
 
-  it ('should return a number', () => {
-    const scoreStrategy = new NullScoreStrategy();
-    const score = scoreStrategy.score;
-    expect(typeof score).toBe('number');
-  });
-
   it ('should return 0', () => {
     const scoreStrategy = new NullScoreStrategy();
     const score = scoreStrategy.score;
