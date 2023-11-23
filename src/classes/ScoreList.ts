1643fd08876bc1a7b7bcf9fb871cf612806293fc
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-23 19:07:01 +0100

    refactor

--- src/classes/ScoreList.ts
+++ src/classes/ScoreList.ts
@@ -10,49 +10,129 @@ import YatzeeScoreStrategy from "./YatzeeScoreStrategy.js";
 import NullScoreStrategy from "./NullScoreStrategy.js";
 
 export default class ScoreList {
-  ones: any;
-  twos: any;
-  threes: any;
-  fours: any;
-  fives: any;
-  sixes: any;
-  aPair: any;
-  twoPairs: any;
-  threePairs: any;
-  threeOfAKind: any;
-  fourOfAKind: any;
-  fiveOfAKind: any;
-  smallStraight: any;
-  largeStraight: any;
-  fullStraight: any;
-  house: any;
-  villa: any;
-  tower: any;
-  chance: any;
-  yatzee: any;
+  #ones: any;
+  #twos: any;
+  #threes: any;
+  #fours: any;
+  #fives: any;
+  #sixes: any;
+  #aPair: any;
+  #twoPairs: any;
+  #threePairs: any;
+  #threeOfAKind: any;
+  #fourOfAKind: any;
+  #fiveOfAKind: any;
+  #smallStraight: any;
+  #largeStraight: any;
+  #fullStraight: any;
+  #house: any;
+  #villa: any;
+  #tower: any;
+  #chance: any;
+  #yatzee: any;
 
   constructor() {
-    this.ones = new NullScoreStrategy();
-    this.twos = new NullScoreStrategy();
-    this.threes = new NullScoreStrategy();
-    this.fours = new NullScoreStrategy();
-    this.fives = new NullScoreStrategy();
-    this.sixes = new NullScoreStrategy();
-    this.aPair = new NullScoreStrategy();
-    this.twoPairs = new NullScoreStrategy();
-    this.threePairs = new NullScoreStrategy();
-    this.threeOfAKind = new NullScoreStrategy();
-    this.fourOfAKind = new NullScoreStrategy();
-    this.fiveOfAKind = new NullScoreStrategy();
-    this.smallStraight = new NullScoreStrategy();
-    this.largeStraight = new NullScoreStrategy();
-    this.fullStraight = new NullScoreStrategy();
-    this.house = new NullScoreStrategy();
-    this.villa = new NullScoreStrategy();
-    this.tower = new NullScoreStrategy();
-    this.chance = new NullScoreStrategy();
-    this.yatzee = new NullScoreStrategy();
+    this.#ones = new NullScoreStrategy();
+    this.#twos = new NullScoreStrategy();
+    this.#threes = new NullScoreStrategy();
+    this.#fours = new NullScoreStrategy();
+    this.#fives = new NullScoreStrategy();
+    this.#sixes = new NullScoreStrategy();
+    this.#aPair = new NullScoreStrategy();
+    this.#twoPairs = new NullScoreStrategy();
+    this.#threePairs = new NullScoreStrategy();
+    this.#threeOfAKind = new NullScoreStrategy();
+    this.#fourOfAKind = new NullScoreStrategy();
+    this.#fiveOfAKind = new NullScoreStrategy();
+    this.#smallStraight = new NullScoreStrategy();
+    this.#largeStraight = new NullScoreStrategy();
+    this.#fullStraight = new NullScoreStrategy();
+    this.#house = new NullScoreStrategy();
+    this.#villa = new NullScoreStrategy();
+    this.#tower = new NullScoreStrategy();
+    this.#chance = new NullScoreStrategy();
+    this.#yatzee = new NullScoreStrategy();
   }
 
+
+  get ones() {
+    return this.#ones.score;
+  }
+
+  get twos() {
+    return this.#twos.score;
+  }
+
+  get threes() {
+    return this.#threes.score;
+  }
+
+  get fours() {
+    return this.#fours.score;
+  }
+
+  get fives() {
+    return this.#fives.score;
+  }
+
+  get sixes() {
+    return this.#sixes.score;
+  }
+
+  get aPair() {
+    return this.#aPair.score;
+  }
+
+  get twoPairs() {
+    return this.#twoPairs.score;
+  }
+
+  get threePairs() {
+    return this.#threePairs.score;
+  }
+
+  get threeOfAKind() {
+    return this.#threeOfAKind.score;
+  }
+
+  get fourOfAKind() {
+    return this.#fourOfAKind.score;
+  }
+
+  get fiveOfAKind() {
+    return this.#fiveOfAKind.score;
+  }
+
+  get smallStraight() {
+    return this.#smallStraight.score;
+  }
+
+  get largeStraight() {
+    return this.#largeStraight.score;
+  }
+
+  get fullStraight() {
+    return this.#fullStraight.score;
+  }
+
+  get house() {
+    return this.#house.score;
+  }
+
+  get villa() {
+    return this.#villa.score;
+  }
+
+  get tower() {
+    return this.#tower.score;
+  }
+
+  get chance() {
+    return this.#chance.score;
+  }
+
+  get yatzee() {
+    return this.#yatzee.score;
+  }
   // TODO: IMPLEMENT SETTERS
 }
