1ad6b325058481409f3bd7a47f67d402352d20e3
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-26 21:32:10 +0100

    refactor

--- src/classes/GameTable.ts
+++ src/classes/GameTable.ts
@@ -1,5 +1,9 @@
 export default class GameTable {
   constructor(numberOfPlayers: number) {
+    this.validateNumberOfPlayers(numberOfPlayers);
+  }
+
+  private validateNumberOfPlayers(numberOfPlayers: number): void {
     if (typeof(numberOfPlayers) !== 'number') {
       throw new Error('The number of players must be a number');
     }
