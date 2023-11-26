cde62fb35369c40e9a6f92741b0df3baab3f4d93
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-26 23:10:01 +0100

    RED: isGameOver should return true if all players have filled their scoreLists

--- test/GameTable.test.js
+++ test/GameTable.test.js
@@ -1,32 +1,33 @@
 import { jest } from '@jest/globals';
 import GameTable from '../serve/compiled-js/classes/GameTable.js';
 import Player from '../serve/compiled-js/classes/Player.js';
+import { mock } from 'node:test';
 
 describe('GameTable', () => {
-  it ('should create a new gameTable', () => {
+  it('should create a new gameTable', () => {
     const gameTable = new GameTable(['000']);
     expect(gameTable).toBeInstanceOf(GameTable);
   });
 
-  it ('should throw an error if the playerNames is not an array', () => {
+  it('should throw an error if the playerNames is not an array', () => {
     expect(() => {
       new GameTable('000');
     }).toThrow("Player names must be an array");
   });
 
-  it ('should throw an error if the playerNames is an empty array', () => {
+  it('should throw an error if the playerNames is an empty array', () => {
     expect(() => {
       new GameTable([]);
     }).toThrow("Player names must not be empty");
   });
 
-  it ('should throw an error if more than 4 players are given', () => {
+  it('should throw an error if more than 4 players are given', () => {
     expect(() => {
       new GameTable(['1', '2', '3', '4', '5']);
     }).toThrow("There can be no more than 4 players");
   });
 
-  it ('should throw an error if the playerNames are not unique', () => {
+  it('should throw an error if the playerNames are not unique', () => {
     expect(() => {
       new GameTable(['1', '1']);
     }).toThrow("Player names must be unique");
@@ -40,7 +41,7 @@ describe('GameTable', () => {
     }).not.toThrow("Player names must be unique");
   });
 
-  it ('should create a field of players with the given names', () => {
+  it('should create a field of players with the given names', () => {
     const gameTable = new GameTable(['111', '222', '333', '444']);
     expect(gameTable.players[0].name).toBe('111');
     expect(gameTable.players[1].name).toBe('222');
@@ -49,22 +50,23 @@ describe('GameTable', () => {
     expect(gameTable.players.length).toBe(4);
   });
 
-  it ('should create a field of six dice', () => {
+  it('should create a field of six dice', () => {
     const gameTable = new GameTable(['111', '222', '333', '444']);
     expect(gameTable.dice.length).toBe(6);
   });
 
-  it ('should have a getter called currentPlayer', () => {
+  it('should have a getter called currentPlayer', () => {
     const gameTable = new GameTable(['111', '222', '333', '444']);
     expect(gameTable.currentPlayer).toBeInstanceOf(Player);
+    expect(gameTable.currentPlayer.name).toBe('111');
   });
 
-  it ('should have a method called advanceToNextPlayer', () => {
+  it('should have a method called advanceToNextPlayer', () => {
     const gameTable = new GameTable(['111', '222', '333', '444']);
     expect(gameTable.advanceToNextPlayer).toBeInstanceOf(Function);
   });
 
-  it ('advanceToNextPlayer should advance the currentPlayer to the next player', () => {
+  it('advanceToNextPlayer should advance the currentPlayer to the next player', () => {
     const gameTable = new GameTable(['111', '222', '333', '444']);
     expect(gameTable.currentPlayer.name).toBe('111');
     gameTable.advanceToNextPlayer();
@@ -77,12 +79,12 @@ describe('GameTable', () => {
     expect(gameTable.currentPlayer.name).toBe('111');
   });
 
-  it ('should have a getter called round', () => {
+  it('should have a getter called round', () => {
     const gameTable = new GameTable(['111', '222', '333', '444']);
     expect(gameTable.round).toBe(1);
   });
 
-  it ('round should increase by 1 after each player has played', () => {
+  it('round should increase by 1 after each player has played', () => {
     const gameTable = new GameTable(['111', '222', '333', '444']);
     expect(gameTable.round).toBe(1);
     for (let i = 0; i < 4; i++) {
@@ -95,9 +97,21 @@ describe('GameTable', () => {
     expect(gameTable.round).toBe(3);
   });
 
-  it ('should have a getter called isGameOver', () => {
+  it('should have a getter called isGameOver', () => {
     const gameTable = new GameTable(['111', '222', '333', '444']);
     expect(gameTable.isGameOver).toBe(false);
+  });
+
+  it('isGameOver should return true if all players have filled their scoreLists', () => {
+    const gameTable = new GameTable(['111', '222', '333', '444']);
+    expect(gameTable.isGameOver).toBe(false);
+
+    const mockAllScoresSet = jest
+      .spyOn(Player.prototype, 'allScoresSet', 'get')
+      .mockImplementation(() => {
+        return true;
+      });
+    expect(gameTable.isGameOver).toBe(true);
   });
 });
 
