b09f92993f52c6b84656f63a189ae0c596b9d56e
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-26 21:13:40 +0100

    RED: Player - should have a scoreList

--- test/Player.test.js
+++ test/Player.test.js
@@ -1,9 +1,16 @@
 import { jest } from '@jest/globals';
 import Player from '../serve/compiled-js/classes/Player.js';
+import ScoreList from '../serve/compiled-js/classes/ScoreList.js';
+
 
 describe('Player', () => {
   it ('should create a new player with a name', () => {
     const player = new Player('Nickname');
     expect(player.name).toBe('Nickname');
+  });
+
+  it ('should have a scoreList', () => {
+    const player = new Player('Nickname');
+    expect(player.scoreList).toBeInstanceOf(ScoreList);
   });
 });
