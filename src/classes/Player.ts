f7c66619d06d9f07632d6c99e29dba99332d425e
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-26 21:18:00 +0100

    GREEN

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,12 +1,19 @@
-export default class Player {
+import ScoreList from "./ScoreList.js";
 
+export default class Player {
   #nickname: String;
+  #scoreList: ScoreList;
 
   constructor(nickname: String){
     this.#nickname = nickname;
+    this.#scoreList = new ScoreList();
   }
 
   get name() : String {
     return this.#nickname;
+  }
+
+  get scoreList() : ScoreList {
+    return this.#scoreList;
   }
 }
