a7b2a478a1ae46d4713207ee7139a8d4aa1dc460
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-26 21:44:34 +0100

    RED: should throw an error if the playerNames is not an array

--- test/GameTable.test.js
+++ test/GameTable.test.js
@@ -6,4 +6,10 @@ describe('GameTable', () => {
     const gameTable = new GameTable(['']);
     expect(gameTable).toBeInstanceOf(GameTable);
   });
+
+  it ('should throw an error if the playerNames is not an array', () => {
+    expect(() => {
+      new GameTable('');
+    }).toThrow();
+  });
 });
