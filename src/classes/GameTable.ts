4867acbf2f007692088db4d69c5f711d10f81c83
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-26 21:41:16 +0100

    RED: GameTable, redesign... out of cheese error, melon melon melon, redo from start

--- src/classes/GameTable.ts
+++ src/classes/GameTable.ts
@@ -1,23 +1,5 @@
 export default class GameTable {
-  constructor(numberOfPlayers: number) {
-    this.validateNumberOfPlayers(numberOfPlayers);
-  }
-
-  private validateNumberOfPlayers(numberOfPlayers: number): void {
-    if (typeof(numberOfPlayers) !== 'number') {
-      throw new Error('The number of players must be a number');
-    }
-
-    if (numberOfPlayers < 1) {
-      throw new Error('The number of players must be greater than 0');
-    }
-
-    if (numberOfPlayers > 4) {
-      throw new Error('The number of players must be less than 5');
-    }
-
-    if (numberOfPlayers % 1 !== 0) {
-      throw new Error('The number of players must be an integer');
-    }
+  constructor() {
+    throw new Error('Not implemented');
   }
 }
--- test/GameTable.test.js
+++ test/GameTable.test.js
@@ -3,23 +3,7 @@ import GameTable from '../serve/compiled-js/classes/GameTable.js';
 
 describe('GameTable', () => {
   it ('should create a new gameTable', () => {
-    const gameTable = new GameTable(1);
+    const gameTable = new GameTable(['']);
     expect(gameTable).toBeInstanceOf(GameTable);
-  });
-
-  it ('should throw an error if the number of players is not a number', () => {
-    expect(() => new GameTable('1')).toThrowError('The number of players must be a number');
-  });
-
-  it ('should throw an error if the number of players is less than 1', () => {
-    expect(() => new GameTable(0)).toThrowError('The number of players must be greater than 0');
-  });
-
-  it ('should throw an error if the number of players is greater than 4', () => {
-    expect(() => new GameTable(5)).toThrowError('The number of players must be less than 5');
-  });
-
-  it ('should throw an error if the number of players is not an integer', () => {
-    expect(() => new GameTable(1.5)).toThrowError('The number of players must be an integer');
   });
 });
