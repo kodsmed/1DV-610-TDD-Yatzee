a6b970f73b5897cfa40bc19539c5abfa1f9a646d
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-26 21:47:02 +0100

    GREEN

--- src/classes/GameTable.ts
+++ src/classes/GameTable.ts
@@ -1,4 +1,11 @@
 export default class GameTable {
   constructor(playerNames: Array<string>) {
+    this.validatePlayerNames(playerNames);
+  }
+
+  private validatePlayerNames(playerNames: unknown) {
+    if (!Array.isArray(playerNames)) {
+      throw new Error("Player names must be an array");
+    }
   }
 }
