e76d16addc0c3158b3fd517cee719a4425436597
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-11-26 21:56:02 +0100

    GREEN

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -22,5 +22,9 @@ export default class Player {
     if (typeof nickname !== "string") {
       throw new Error("Player name must be a string");
     }
+
+    if (nickname.length < 1) {
+      throw new Error("Player name must not be empty");
+    }
   }
 }
