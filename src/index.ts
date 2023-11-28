fe71adefac5f6638568da71a817aebc0143401b6
Adam Sjöström <as223js@student.lnu.se>
2023-11-29 00:35:06 +0100

    GREEN

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,88 @@
+import Player from "./classes/Player.js";
+import ScoreList from "./classes/ScoreList.js"
+import Die from "./classes/DieClass.js";
+
 export default class GameEngine{
+  #players : Array<Player>;
+  #clickListener : EventListener;
+
+  constructor() {
+    this.#players = [];
+    this.#clickListener = this.#clickHandler.bind(this);
+  }
+
   prepareGame(){
     const playerNameInputField = document.querySelector('#playerName') as HTMLInputElement
     if (playerNameInputField == null) {
       throw new Error('the input filed #playerName is missing in index.html')
     }
     playerNameInputField.style.display = 'none'
-  
+
     const buttonElement = document.querySelector('button') as HTMLInputElement
     if (buttonElement == null) {
       throw new Error('the button element is missing in index.html')
     }
     buttonElement.textContent = 'Start'
 
-    buttonElement.addEventListener('click', () => {
-      const numberOfPlayersSelect = document.querySelector('#numberOfPlayers') as HTMLSelectElement; // Select the dropdown
+    buttonElement.addEventListener('click', this.#clickListener);
 
-      const selectedValue = numberOfPlayersSelect.value; // Get the selected value
-
-      const newGameEvent = new window.CustomEvent('startNewGame', { 
-          bubbles: true, 
-          composed: true, 
-          detail: selectedValue  // Use the selected value for the detail
-      });
-      window.dispatchEvent(newGameEvent);
-      this.startGame();
+    window.addEventListener('startNewGame', (event) => {
+      const myEvent = event as CustomEvent;
+      this.getPlayerNames(myEvent.detail)
     });
   }
 
-  startGame() {
+  #clickHandler() {
+    const numberOfPlayersSelect = document.querySelector('#numberOfPlayers') as HTMLSelectElement; // Select the dropdow
+    const selectedValue = numberOfPlayersSelect.value; // Get the selected value
+    const newGameEvent = new window.CustomEvent('startNewGame', {
+        bubbles: true,
+        composed: true,
+        detail: selectedValue  // Use the selected value for the detail
+    });
+    window.dispatchEvent(newGameEvent);
+  }
+
+  getPlayerNames(numberOfPlayers: number) {
     const playerNameInputField = document.querySelector('#playerName') as HTMLInputElement
-
     playerNameInputField.style.display = 'block'
-    
-    const selectionField = document.querySelector('#numberOfPlayers') as HTMLInputElement
 
+    const selectionField = document.querySelector('#numberOfPlayers') as HTMLInputElement
     selectionField.style.display = 'none'
+
+    const submitButton = document.querySelector('button') as HTMLButtonElement
+    submitButton.textContent = 'Submit player1s name'
+    submitButton.removeEventListener('click', this.#clickListener)
+    this.#clickListener = this.#addPlayerHandler.bind(this, numberOfPlayers);
+    submitButton.addEventListener('click', this.#clickListener)
+  }
+
+  #addPlayerHandler(numberOfPlayers: number) {
+    event?.preventDefault()
+    const submitButton = document.querySelector('button') as HTMLButtonElement
+    submitButton.disabled = true
+    const playerNameInputField = document.querySelector('#playerName') as HTMLInputElement
+    const enteredName = playerNameInputField.value
+
+    try {
+      const playerToAdd = new Player(enteredName)
+      this.#players.push(playerToAdd)
+    } catch(error) {
+      // if (error instanceof Error) {
+      //   alert(error.message);
+      // }
+      playerNameInputField.value = "";
+      return
+    }
+
+    if (this.#players.length < numberOfPlayers) {
+      console.log(this.#players.length)
+      submitButton.textContent = `Submit player${this.#players.length + 1}s name`;
+    } else {
+      submitButton.removeEventListener('click', this.#clickListener);
+      submitButton.style.display = 'none';
+    }
+    submitButton.disabled = false
   }
 }
 
