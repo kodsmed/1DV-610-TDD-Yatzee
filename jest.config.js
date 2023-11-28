2ddd8f6635187035b02717949221efe8877e1d36
Adam Sjöström <as223js@student.lnu.se>
2023-11-28 15:12:16 +0100

    fix TextEncode problem

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
+import { TextEncoder, TextDecoder } from "util";
 export default {
+  globals: {
+    "ts-jest": {
+       tsConfigFile: "tsconfig.json"
+     },
+     TextEncoder: TextEncoder,
+     TextDecoder: TextDecoder
+  },
   testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['js', 'mjs'],
   testMatch: ['**/test/**/*.test.js'],
