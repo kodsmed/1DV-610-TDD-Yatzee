2a4f504a1b6b8bb8d3238464519a80cb93316720
Adam Sjöström <as223js@student.lnu.se>
2023-11-28 15:50:49 +0100

    RED: should hide the name input until number of players have been selected

--- test/IndexJs.test.js
+++ test/IndexJs.test.js
@@ -1,15 +1,36 @@
 import { JSDOM } from 'jsdom';
+import fs from 'fs';
+import path, { dirname }  from 'path';
 
 describe('index.js', () => {
   let htmlDoc;
+  let window;
 
   beforeAll(async () => {
-    const htmlDom = await JSDOM.fromFile('./serve/index.html');
-    htmlDoc = htmlDom.window.document;
-  })
+    const htmlDom = await JSDOM.fromFile('./serve/index.html', { 
+      runScripts: "dangerously",
+      resources: "usable",
+    });
+
+    window = htmlDom.window;
+    htmlDoc = window.document;
+
+    const scriptPath = path.resolve('./serve/compiled-js/index.js');
+    const scriptContent = fs.readFileSync(scriptPath, 'utf-8');
+    const scriptEl = htmlDoc.createElement('script');
+    scriptEl.textContent = scriptContent;
+    htmlDoc.body.appendChild(scriptEl);
+
+    // Wait for the script to be executed
+    await new Promise((resolve) => {
+      window.onload = resolve;
+    });
+
+    htmlDoc = window.document;
+  });
 
   it('should hide the name input until number of players have been selected', () => {
     const inputElement = htmlDoc.querySelector('#playerName');
     expect(inputElement.style.display).toBe('none');
-  })
-})
+  });
+});
