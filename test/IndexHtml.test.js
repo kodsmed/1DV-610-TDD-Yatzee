bad1c17cea3983de4cbb1a329b06cb0e6517b1c0
Adam Sjöström <as223js@student.lnu.se>
2023-11-28 13:59:00 +0100

    RED: the select element should have the values and text content 1..4

--- test/IndexHtml.test.js
+++ test/IndexHtml.test.js
@@ -40,4 +40,20 @@ describe('index.html', () => {
       expect(element.tagName.toLowerCase()).toBe('select');
     })
   })
+
+  it('the select element should have the values and text content 1..4', () => {
+    const selectElement = document.querySelector('#numberOfPlayers');
+    const options = selectElement.querySelectorAll('option');
+    expect(options.length).toBe(4);
+    let unexpectedTextContentDetected = false;
+    for (let index = 0; index < options.length; index++) {
+      if (options[index].textContent != `${index + 1} players`) {
+        console.log('enountered unexpected option text in select element:' + options[index].textContent)
+        unexpectedTextContentDetected = true;
+      }
+    }
+
+    expect(unexpectedTextContentDetected).toBe(false);
+    
+  })
 });
